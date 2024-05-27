a5fef67316eef6624222dee3eb7f5e96872a3e04
matthewlindgren <lindmatt@umich.edu>
2024-05-27 15:14:50 -0400

    initial commit, scrolling animation working

---
+++ js/script.js
@@ -0,0 +1,30 @@
+// on load
+const headerImage = document.querySelector('.header-img');
+headerImage.style.marginBottom = "100vh";
+
+const windowHeight = window.innerHeight;
+const halfWindowHeight = windowHeight / 2;
+const scrollStart = getElementMidPosition(headerImage) - (halfWindowHeight)
+document.addEventListener('scroll', function() {
+    const scrollPosition = window.scrollY;
+
+    if (scrollPosition < scrollStart) {
+        headerImage.style.position = "static";
+        headerImage.style.transform = "none";
+    }
+    else if (scrollPosition <= headerImage.height + scrollStart + 100) {
+        headerImage.style.position = "sticky";
+        headerImage.style.top = halfWindowHeight + "px";
+        headerImage.style.transform = "translateY(-50%)";
+        headerImage.style.clipPath = "inset(" + Math.ceil(20 - ((scrollPosition - scrollStart) / (headerImage.height) * 20)) + "%)";
+    } 
+    else {
+        headerImage.style.clipPath = "inset(0)"
+        headerImage.style.top = -1 * (scrollPosition - (headerImage.height + scrollStart) - 100) + "px";
+        headerImage.style.transform = "translateY(0)";
+    }
+});
+
+function getElementMidPosition(element) {
+    return element.getBoundingClientRect().top + window.scrollY + (element.height / 2);
+}
